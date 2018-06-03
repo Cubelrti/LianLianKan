a06264c04188ddd2831b67ffa1e310e42caf8ce6
Xiang Chaozhu <XiangCZ929@Gmail.com>
2018-06-03 15:32:13 +0800

    fix lightning

--- LianLianKan/LianLianKan.cpp
+++ LianLianKan/LianLianKan.cpp
@@ -244,8 +244,8 @@ void LianLianKan::navigateGame() {
 }
 
 void LianLianKan::drawLightning(std::vector<std::vector<int>> seq, std::vector<int> lightningSeq) {
-	if (seq.size() == 0 || lightningSeq.size() == 0) return;
 	lightningSequence.clear();
+	if (seq.size() == 0 || seq.size() == 0 || lightningSeq.size() == 0) return;
 	QSound::play("./Sounds/Elec.wav");
 	int count = 1;
 	for (int i = 0; i < lightningSeq.size(); i++) {
